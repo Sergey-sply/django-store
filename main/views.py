b16335bea38ba65e25e79228a181d24ab9b09c59
sergey-sply <sergeysedov12@gmail.com>
2024-01-05 04:17:09 +0300

    add templates and css files

--- main/views.py
+++ main/views.py
@@ -4,12 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     context: dict = {
-        "title": "Home", 
-        "content": "Главная страница - Home"
+        'title': 'Home', 
+        'content': 'Главная страница - Home'
         }
 
-    return render(request, "main/index.html", context)
+    return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse("About Page")
+    context: dict = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Тестовый текст для проверки работы шаблона йцукенгшщзхъфывапролджэячсмитьбю'
+    }
+
+    return render(request, 'main/about.html', context)
