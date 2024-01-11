cd31c3b734b472cb2c0b39f98777359842e2a488
sergey-sply <sergeysedov12@gmail.com>
2024-01-12 01:20:37 +0300

    add sorting by category

--- products/views.py
+++ products/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from products.models import Products
 
-def catalog(request):
+def catalog(request, category_slug):
     """For displaying all products"""
 
-    products = Products.objects.all() # get all prod from bd 'products'
-
+    if category_slug == 'all':
+        products = Products.objects.all() # get all prod from bd 'products'
+    else:
+        products = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+        
     context = {
         "title": "Home - Каталог",
         "products": products,
