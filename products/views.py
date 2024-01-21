336c7edec188a437a39a705a56ed54c205f12f36
sergey-sply <sergeysedov12@gmail.com>
2024-01-21 22:31:00 +0300

    add searching by description and name of prod, working with Q-object

--- products/views.py
+++ products/views.py
@@ -2,17 +2,21 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 
 from products.models import Products
+from products.utils import q_search
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     """For displaying all products"""
     
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
 
     if category_slug == 'all':
         products = Products.objects.all() # get all prod from bd 'products'
+    elif query:
+        products = q_search(query)
     else:
         products = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
