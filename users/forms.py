47056d249de02ada84366fdf3e0990987e04a37b
sergey-sply <sergeysedov12@gmail.com>
2024-01-24 13:29:29 +0300

    complete work with login form and view

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
 
+
 class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField()
+
+    password = forms.CharField()
+
     class Meta:
         model = User
+        fields = ['username', 'password']
+        
