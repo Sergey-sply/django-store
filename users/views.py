92ee493f16f575bb965a888e443f7358cef4ea0a
sergey-sply <sergeysedov12@gmail.com>
2024-01-24 23:22:34 +0300

    Add registration func: ready views, registration.html, form

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -30,8 +30,19 @@ def login(request):
 
 def registration(request):
 
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()    # save data from forms to bd
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
         'title': 'Регистрация',
+        'form': form,
     }
 
     return render(request, 'users/registration.html', context)
@@ -48,8 +59,5 @@ def profile(request):
 
 def logout(request):
 
-    context = {
-        'title': 'Выход',
-    }
-
-    return render(request, '', context)
+    auth.logout(request)
+    return redirect(reverse('main:index'))
