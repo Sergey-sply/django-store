8aaffaf38b31d2361d6b3dd8ff9dc49c9f85b149
sergey-sply <sergeysedov12@gmail.com>
2024-01-25 21:30:08 +0300

    add redirect to target page

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -146,4 +146,6 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/user/login/'
