724a4885dab7c0d7b79c078395674409bd99024d
sergey-sply <sergeysedov12@gmail.com>
2024-02-01 20:18:29 +0300

    change admin panel (carts, products, users)

--- products/admin.py
+++ products/admin.py
@@ -2,13 +2,24 @@ from django.contrib import admin
 
 from products.models import Categories, Products
 
-#admin.site.register(Categories)
-#admin.site.register(Products)
-
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name',]
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'quantity', 'price', 'discout']
+    list_editable = ['discout',]
+    search_fields = ['name', 'discription']
+    list_filter = ['discout', 'quantity', 'category']
+    fields = [
+        'name',
+        'category',
+        'slug',
+        'description',
+        'image',
+        ('price', 'discout'),
+        'quantity',
+    ]
