b9c2bd103eb235b80116d35f86eeb8da6f641eb9
sergey-sply <sergeysedov12@gmail.com>
2024-02-01 21:25:08 +0300

    create orders app & create models for orders

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('catalog/', include('products.urls', namespace='catalog')),
     path('user/', include('users.urls', namespace='user')),
     path('cart/', include('carts.urls', namespace='cart')),
+    path('order/', include('orders.urls', namespace='order')),
 ]
 
 if settings.DEBUG:
